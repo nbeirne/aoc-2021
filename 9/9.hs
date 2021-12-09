bb50fb7f89f6058646cebc0f7e7fa515b4d3d280
Nick <6374684+nbeirne@users.noreply.github.com>
2021-12-09 12:28:29 -0500

    fast and dirty for day 9

--- 9/9.hs
+++ 9/9.hs
@@ -1,5 +1,10 @@
 import Control.Lens
+import Control.Monad
 import Data.Maybe
+import Data.List
+
+import qualified Data.Set as Set
+import Data.Set (Set)
 
 parse :: String -> [[Int]]
 parse = map (map read . map (:[])) . lines
@@ -8,26 +13,28 @@ type Point = (Int,Int)
 type Board = [[Int]]
 
 m :: Board
-m = [[1,2,3],
+m = [[1,2,1],
      [4,5,6],
      [7,8,9]]
 
 val :: Board -> Point -> Int
 val b (x,y) = (b !! x) !! y
 
-valS :: Board -> Point -> Maybe Int
-valS b (x,y) = (b ^? element x) >>= (\b' -> b' ^? element y)
+valSafe :: Board -> Point -> Maybe Int
+valSafe b (x,y) = (b ^? element x) >>= (\b' -> b' ^? element y)
 
-adj :: Board -> Point -> [Int]
-adj bd (x,y) = [a,b,c,d] >>= id
-  where a = maybe [] (:[]) $ valS bd (x-1,y)
-        b = maybe [] (:[]) $ valS bd (x+1,y)
-        c = maybe [] (:[]) $ valS bd (x,y-1)
-        d = maybe [] (:[]) $ valS bd (x,y+1)
+adjPts (x,y) = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+
+adjVals :: Board -> Point -> [(Point,Int)]
+--adjVals bd pt = map (maybe [] (:[]) . valSafe bd) (adjPts pt) >>= id
+adjVals bd pt = do
+  pt' <- adjPts pt
+  guard $ not $ Nothing == valSafe bd pt'
+  return (pt', val bd pt')
 
 isLowPoint b p = all (\pt -> pt > v) a
   where v = val b p
-        a = adj b p
+        (_,a) = unzip $ adjVals b p
 
 
 findLowPoints :: Board -> [Point]
@@ -36,14 +43,34 @@ findLowPoints b = pts
 
 
 solve1 b = map (val b) $ findLowPoints b
-solve2 = id
+
+findBasinR b (set, basin) 
+  | null set = basin
+  | otherwise = findBasinR b (Set.union unseenPts ps, b')
+  where p = Set.elemAt 0 set -- get point
+        ps = Set.deleteAt 0 set -- remove that point
+        adjs = adjVals b p -- get adjasent
+        (includePts,_) = unzip $ filter ((/=) 9 . snd) adjs -- filter out points which are lt pointval
+        unseenPts = Set.difference (Set.fromList includePts) basin
+        b' = if includePts /= [] then Set.insert p basin else basin -- add p if this is not the top point
+
+
+findBasin :: Board -> Point -> Set Point
+findBasin b lowPoint = findBasinR b (Set.singleton lowPoint, Set.empty)
+
+solve2 b = map (findBasin b) lowPoints
+  where lowPoints = findLowPoints b
+        
+  
 
 -- score a solution based on the id
 score1 = sum . map (+1)
-score2 = const 0
+score2 = product . take 3 . reverse . sort . map Set.size
+--score2 = Set
 
 test1a = 15
-test2a = 0
+--test2a = 14
+test2a = 1134
 
 -- boiler plate
 
